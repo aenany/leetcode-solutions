37e1c3feb949c19b354c6e79807913133ca5a225
A Enany <a.enany91@gmail.com>
2017-09-12 23:31:40 -0400

    fix spacing

--- js/stack-using-queues.js
+++ js/stack-using-queues.js
@@ -8,18 +8,15 @@ MyStack.prototype.push = function(x) {
 
 MyStack.prototype.pop = function() {
     var queue = this.queue;
-
 	return queue.splice(queue.length -1, 1)[0];
 };
 
 MyStack.prototype.top = function() {
     var queue = this.queue;
-
 	return queue[queue.length -1];
 };
 
 MyStack.prototype.empty = function() {
     var queue = this.queue;
-
 	return queue.length === 0  ? true : false;
 };
