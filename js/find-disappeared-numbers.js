bd73ecf4cc34da3ef655224028648ebd729973d5
A Enany <a.enany91@gmail.com>
2017-09-13 19:21:37 -0400

    base cases?

--- js/find-disappeared-numbers.js
+++ js/find-disappeared-numbers.js
@@ -3,8 +3,20 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-    var result = [];
-    nums.sort(function(a, b){return a - b});    
-    for(var i=1; i <= nums[nums.length-1]; i++){ result.push(i); }
-    return result.filter(function(e){ nums.indexOf(e) < 0; });
+	var result = [];
+
+	if (nums.length == 2 && nums[0] != nums[1]) {
+	
+		result = [nums[1]+1]; 
+		return result; 
+	}
+
+	if (nums.length == 1 ) { result = [nums[0]]; return result; }
+	if (nums.length == 0 ) { result = nums; return result; }
+
+	else {
+		nums.sort(function(a, b){return a - b;});    
+		for(var i=1; i <= nums[nums.length-1]; i++){ result.push(i); }
+		return result.filter(function(e){ return !nums.includes(e); });    
+	}
 };
